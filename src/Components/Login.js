59422f34a9a5e92ab86fbbbcbf05ba442a9b6752
Entropiks <mikecorseri@gmail.com>
2020-11-30 08:11:03 -0800

    grid refactor

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import KitchenIcon from '@material-ui/icons/Kitchen';
 import FacebookIcon from '@material-ui/icons/Facebook';
-import TextField from '@material-ui/core/TextField';
+// import TextField from '@material-ui/core/TextField';
 
 import { auth, provider } from '../firebase';
 
@@ -18,34 +18,24 @@ function Login() {
     return (
         <div className="loginPanel">
           <div class="panel_left">
-            <div className="row login_logo">
+            <div className="login_logo">
               <KitchenIcon />
               <h2>PANTRY</h2>
               
             </div>
             <div className="login_header">
               <h1>Welcome, Please login or signup for a new account!</h1>
+              <btn onClick={user_login} className="btn btn_facebook"><FacebookIcon />Login with Facebook</btn>&nbsp;
             </div>
-            <div className="login_body">
-              <button onClick={user_login} className="btn btn_facebook"><FacebookIcon />Login with Facebook</button>&nbsp;
-              {/* <btn className="btn btn_google">Login with something else</btn> */}
-            
-            </div>
-            <div className="login_form">
-              <form noValidate autoComplete="off">
-                  <TextField id="outlined-basic" label="Email Address" variant="outlined" /><br></br>
-                  <TextField id="outlined-basic" label="Password" variant="outlined" />
-              </form>
-            </div>
-            <div className="row login_footer">
+  
+            <div className="login_footer">
                 <p>By signing up, you agree to Pantry's <br></br>Terms and Conditions + Privacy Policy</p>
             </div>
         </div>
 
-        
         <div class="panel_right">
           <div className="login_intro">
-            <h1>Welcome to Pantry</h1>
+            <h1>HOW THIS WORKS...</h1>
             <p>Here's how it works... Create your ingredient / grocery list and we will give you a curated list of meals and snacks you can already make.</p> 
           </div>
 
