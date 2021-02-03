168cc4c57b840e68ba4994196947b172d8f37fe7
Entropiks <mikecorseri@gmail.com>
2021-02-03 10:13:48 -0800

    complete UI strip, going mobile first

--- src/Components/Dash.js
+++ src/Components/Dash.js
@@ -1,13 +1,17 @@
 import React, { useState } from 'react';
 import './Dash.css';
 import KitchenIcon from '@material-ui/icons/Kitchen';
+import SingleIngredient from './IngredientList';
 
 function Dash() {
 
   const [recipes, setRecipes] = useState([]);
   const [input, setInput] = useState('');
+
   const addItem = (event) => {
-    setRecipes([...recipes, input])
+    event.preventDefault();
+    setRecipes([...recipes, input]);
+    setInput('');
   }
 
   return (
@@ -20,10 +24,18 @@ function Dash() {
         <div className="startRecipeCard">
           <h1>What ingredients do you have?</h1>
           <p>We realize this may take you a few minutes, but trust us - great meal ideas incoming. We realize this may take you a few minutes, but trust us - great meal ideas incoming.</p>
-          <input className="user_input" value={input} onChange={event => setInput(event.target.value)} />
-          <button onClick={addItem}>Add</button>
+          <form>
+            <input className="user_input" value={input} onChange={event => setInput(event.target.value)} />
+            <button type="submit" onClick={addItem}>Add</button>
+          </form>
         </div>
       </div>
+      {/* Needs to route item name and info */}
+      {/* <div className="ingredientWrapper">
+        {recipes.map(recipe => (
+          <SingleIngredient>{recipe}</SingleIngredient>
+        ))}
+      </div> */}
       <div className="ingredientWrapper">
         {recipes.map(recipe => (
           <div className="ingredient_name">
--- src/Components/IngredientList.js
+++ src/Components/IngredientList.js
@@ -18,4 +18,4 @@ function IngredientList() {
     )
 }
 
-export default IngredientList
+export default IngredientList;
